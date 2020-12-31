46b58d7343744dce70df4167b12664c72e590306
Amr <amr.hazem.mahmoud@gmail.com>
2020-12-31 20:49:58 +0200

    HERE-002  add axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { generateToken } from "./actions/authenticationActions";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // var platform = new H.service.Platform({
+  //   apikey: "1NASa-QTGazkqwwCOZjMjqgLCLGmBTAvSB4afPNzMbQ",
+  // });
+  generateToken();
+  fetch(
+    `https://discover.search.hereapi.com/v1/discover?apiKey=3jrqPg29rurQRzN_TL2cioMVYHodcbeTUwaw_GqFH-A&lang=en-US&q=petrol+station&at=52.5228,13.4124`
+  ).then((res) => console.log("fetch", res.json(), res.body));
+  return <div className="App">here</div>;
 }
 
 export default App;
