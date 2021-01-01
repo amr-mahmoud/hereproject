048dc1f7b5ca4cde30e51915f1de0621d0a8a459
Amr <amr.hazem.mahmoud@gmail.com>
2021-01-01 23:32:07 +0200

    HERE-003 add Api

--- src/actions/locationApi.js
+++ src/actions/locationApi.js
@@ -1,6 +1,14 @@
 import axios from "axios";
-export const getLocationData = () => {
-  fetch(
-    `https://discover.search.hereapi.com/v1/discover?apiKey=3jrqPg29rurQRzN_TL2cioMVYHodcbeTUwaw_GqFH-A&lang=en-US&q=petrol+station&at=52.5228,13.4124`
-  ).then((res) => console.log("fetch", res.json(), res.body));
+
+const apiKey = "4CwEQaxA5OnFpekbxDT-I096-13RkgrY5tYxtZMnPWY";
+
+export const getLocationData = async (input) => {
+  let updatedInput = input;
+  for (let index = 0; index < input.length; index++) {
+    let data = [input[index].Latitude, input[index].Longitude];
+    let result = await axios.get(
+      `https://revgeocode.search.hereapi.com/v1/revgeocode?apiKey=${apiKey}&at=${data}&lang=en-US`
+    );
+    updatedInput[index].address = result.data.items[0].address.label;
+  }
 };
