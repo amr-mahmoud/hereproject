32db7c03e283194c4553ec4e2bfdee3650ff4934
Amr <amr.hazem.mahmoud@gmail.com>
2021-01-02 04:15:44 +0200

    HERE-008 add the map for the locations

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import "./App.css";
 import React, { useState } from "react";
 import DropBox from "./components/DropBox/DropBox";
 import Header from "./components/Header/Header";
+import { DisplayMapFC } from "./components/DisplayMapFc/DisplayMapFc";
 import MembersList from "./components/MembersList/MembersList";
 import { Sections } from "./constants/constants";
 
 const App = () => {
   const [list, setList] = useState([]);
-  const [section, setSection] = useState(Sections.upload);
+  const [section, setSection] = useState(Sections.Map);
 
   console.log("List", list);
   return (
     <div className="App">
       <Header setSection={setSection} />
       <main>
-        {section === Sections.membersList && <MembersList list={list} />}
+        {section === Sections.Map && <DisplayMapFC list={list} />}
+        {section === Sections.List && <MembersList list={list} />}
         {section === Sections.upload && <DropBox setList={setList} />}
       </main>
       <footer>Thank you</footer>
