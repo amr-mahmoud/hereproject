17148f2e24a7276de33452536dee5b2f9b6e4a0c
Amr <amr.hazem.mahmoud@gmail.com>
2021-01-02 03:20:26 +0200

    HERE-008 app js state  modification

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
 import React, { useState } from "react";
 import DropBox from "./components/DropBox/DropBox";
+import Header from "./components/Header/Header";
+import MembersList from "./components/MembersList/MembersList";
+import { Sections } from "./constants/constants";
 
 const App = () => {
   const [list, setList] = useState([]);
+  const [section, setSection] = useState(Sections.upload);
 
   console.log("List", list);
   return (
     <div className="App">
-      <header>Title</header>
+      <Header setSection={setSection} />
       <main>
-        <DropBox />
+        {section === Sections.membersList && <MembersList list={list} />}
+        {section === Sections.upload && <DropBox setList={setList} />}
       </main>
       <footer>Thank you</footer>
     </div>
